ef77030775ac897eb3d7699c828a85fb3b17b177
Mahmud <mahmudabdullahal707@gmail.com>
2026-08-01 13:37:28 -0400

    Fix gh-pages blank screen base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   const isProd = mode === 'production';
   return {
-    base: '/',
+    base: '/aeirmist/',
     plugins: [react(), tailwindcss()],
     define: {
     },
